c0dd7cc8a169c7373b61e9e9a6cc5af93dde659e
ebwinters@comcast.net <ebwinters@comcast.net>
2019-07-10 20:27:30 -0700

    arrow key movement with special moves for top bottom L and R

--- kilo.c
+++ kilo.c
@@ -12,6 +12,17 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP, 
+	ARROW_DOWN,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
 /*** data ***/
 struct editorConfig {
 	int cx, cy;
@@ -57,7 +68,7 @@ void enableRawMode() {
 	}
 }
 
-char editorReadKey() {
+int editorReadKey() {
 	int nread;
 	char c;
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -65,7 +76,65 @@ char editorReadKey() {
 			die("read");
 		}
 	}
-	return c;
+	if (c == '\x1b') {
+		char seq[3];
+
+		//check for timeout in first chars
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+			return '\x1b';
+		}
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+			return '\x1b';
+		}
+
+		//check if first two chars make arrow esc sequence
+		//then alias to WASD keys
+		if (seq[0] == '[') {
+			//if second byte is digit, we expect ~ as third
+			//representing page up/down or home/end depending on digit
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) {
+					return '\x1b';
+				}
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '1':
+							return HOME_KEY;
+						case '4':
+							return END_KEY;
+						case '5':
+							return PAGE_UP;
+						case '6':
+							return PAGE_DOWN;
+						case '7':
+							return HOME_KEY;
+						case '8':
+							return END_KEY;
+					}
+				}
+			}
+			else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
+			}
+		}
+		else if (seq[0] == 'O') {
+			switch (seq[1]) {
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+		}
+		return '\x1b';
+	}
+	else {
+		return c;
+	}
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -196,6 +265,7 @@ void editorRefreshScreen() {
 
 	//move cursor
 	char buff[32];
+	//terminal uses 1 indexed vals
 	snprintf(buff, sizeof(buff), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
 	abAppend(&ab, buff, strlen(buff));
 	
@@ -206,25 +276,34 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
 	switch(key) {
-		case 'a':
-			E.cx--;
+		//Logic in each case to make sure cursor stays on screen
+		case ARROW_LEFT:
+			if (E.cx != 0) {
+				E.cx--;
+			}
 			break;
-		case 'd':
-			E.cx++;
+		case ARROW_RIGHT:
+			if (E.cx != E.screencols - 1) {
+				E.cx++;
+			}
 			break;
-		case 'w':
-			E.cy--;
+		case ARROW_UP:
+			if (E.cy != 0) {
+				E.cy--;
+			}
 			break;
-		case 's':
-			E.cy++;
+		case ARROW_DOWN:
+			if (E.cy != E.screenrows - 1) {
+				E.cy++;
+			}
 			break;
 	}
 }
 
 void editorProcessKeyPress() {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -233,10 +312,29 @@ void editorProcessKeyPress() {
 			exit(0);
 			break;
 
-		case 'w':
-		case 's':
-		case 'a':
-		case 'd':
+		//home and end move all the way l/r respectively
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				//move cursor to top or bottom of screen for page up/down
+				int times = E.screenrows;
+				while (times--) {
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+				}
+			}
+			break;
+
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
 			editorMoveCursor(c);
 			break;
 	}
